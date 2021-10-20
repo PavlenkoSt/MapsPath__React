071b6be996e838681d7a7b3b5c13d45d76ad5e6c
Stanislav <verbatim8872@gmail.com>
2021-10-20 16:18:31 +0300

    add list

--- src/components/Header/HeaderContent.tsx
+++ src/components/Header/HeaderContent.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button } from 'antd'
 import s from './HeaderContent.module.scss'
+import { FullscreenOutlined } from '@ant-design/icons'
 
 const HeaderContent = () => {
   return (
     <div className={s.content}>
       <div className={s.logo}>
-        <img src="/icons/expand.svg" alt="logo" />
+        <FullscreenOutlined />
         <span>Saunter</span>
       </div>
       <Button type="primary">Add path</Button>
--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,9 +1,34 @@
 import Search from 'antd/lib/input/Search'
 import React, { useState } from 'react'
+import { FullscreenOutlined, RightOutlined } from '@ant-design/icons'
 import s from './Routes.module.scss'
+import { List } from 'antd'
 
 const Routes = () => {
   const [searchValue, setSearchValue] = useState('')
+
+  const mock = [
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+    { id: 0, title: 'title', desc: 'desc', distance: '12 km' },
+  ]
 
   const onSearch = () => {
     console.log(searchValue)
@@ -11,7 +36,24 @@ const Routes = () => {
 
   return (
     <div className={s.routes}>
-      <Search value={searchValue} onChange={e => setSearchValue(e.target.value)} onSearch={onSearch} width="100%" />
+      <Search
+        value={searchValue}
+        onChange={e => setSearchValue(e.target.value)}
+        onSearch={onSearch}
+        className={s.input}
+      />
+      <List
+        dataSource={mock}
+        className={s.list}
+        renderItem={route => (
+          <List.Item key={route.id} className={s.route}>
+            <FullscreenOutlined className={s.icon} />
+            <List.Item.Meta title={<a href="https://ant.design">{route.title}</a>} description={route.desc} />
+            <div className={s.distance}>{route.distance}</div>
+            <RightOutlined className={s.arr} />
+          </List.Item>
+        )}
+      />
     </div>
   )
 }
