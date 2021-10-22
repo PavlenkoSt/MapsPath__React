05cccaa304470461f7bb88a2210a284684d571b5
Stanislav <verbatim8872@gmail.com>
2021-10-22 09:23:24 +0300

    add firebase connect data to .env

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,15 +1,26 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from '@firebase/firestore'
 
+const {
+  REACT_APP_FIREBASE_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DB_URL,
+  REACT_APP_PROJ_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESS_SENDER_ID,
+  REACT_APP_ID,
+  REACT_APP_MEASUREMENT_ID,
+} = process.env
+
 const firebaseApp = initializeApp({
-  apiKey: 'AIzaSyCtKfb4UbEmk7QQkr23nBq1BaGpK1zVjKs',
-  authDomain: 'routes-8f6ec.firebaseapp.com',
-  databaseURL: 'https://routes-8f6ec-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'routes-8f6ec',
-  storageBucket: 'routes-8f6ec.appspot.com',
-  messagingSenderId: '147611270023',
-  appId: '1:147611270023:web:7adf48c297b86fb88db122',
-  measurementId: 'G-GK40M9MQN6',
+  apiKey: REACT_APP_FIREBASE_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DB_URL,
+  projectId: REACT_APP_PROJ_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESS_SENDER_ID,
+  appId: REACT_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 })
 
 export const db = getFirestore(firebaseApp)
